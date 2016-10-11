443e8419e8d64ef1fcb8e961b854b10a5f861897
Gabriel L. Viloria <gviloria@hawaii.edu>
2016-10-11 11:06:27 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -5,12 +5,24 @@ PlayersList = new Mongo.Collection('players');
 
 if(Meteor.isClient){
   console.log("Hello client");
+
   Template.leaderboard.helpers({
     'player': function(){
       return PlayersList.find();
     },
-    'otherHelperFunction': function(){
-      return "Some other function";
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+      }
+    }
+  });
+
+  Template.leaderboard.events({
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
     }
   });
 }

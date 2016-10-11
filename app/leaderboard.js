36bcc0e045e7c26971f03288f17c5de3551762ff
Gabriel L. Viloria <gviloria@hawaii.edu>
2016-10-11 10:13:13 -1000

    Task 4: Templates Chapter

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -3,3 +3,18 @@
  */
 PlayersList = new Mongo.Collection('players');
 
+if(Meteor.isClient){
+  console.log("Hello client");
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'otherHelperFunction': function(){
+      return "Some other function";
+    }
+  });
+}
+
+if(Meteor.isServer){
+  console.log("Hello server");
+}
